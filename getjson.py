cbfd5d7d0d35e7b8d573b4226d3df8a8597ce07d
Ther <1329438302@qq.com>
2022-06-25 17:44:34 +0800

    chore: change yaml files

--- getjson.py
+++ getjson.py
@@ -1,8 +1,7 @@
 import torch
-import fairseq
 import json
 
-data2vec = torch.load("/mnt/c/Users/86181/Desktop/checkpoint_423_400000.pt")
+data2vec = torch.load("/home/nullptr/open-source/fairseq/models/checkpoint_423_400000.pt")
 cfg = data2vec['cfg']
 with open("data2vec.json", 'w') as f:
     f.write(json.dumps(cfg, indent=4, sort_keys=True))
